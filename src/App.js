c1532c9a5536e2a36e1620b7ef17a2114daf6ead
dilshad09 <dilshad_fw11_177@masai.school>
2022-03-09 18:45:27 +0530

    counter with useReducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
 import React from 'react';
-import  UseState from './components/useState/UseState'
-import UseEffect from './components/useEffect/UseEffect';
+// import  UseState from './components/useState/UseState'
+// import UseEffect from './components/useEffect/UseEffect';
+import UseReducer from './components/useReducer/UseReducer';
 function App() {
 
   return (
     <div className="App">
     {/* <UseState/> */}
-    <UseEffect/>
+    {/* <UseEffect/> */}
+    <UseReducer/>
     </div>
   );
 }
---
+++ src/components/useReducer/UseReducer.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useReducer, useState } from "react";
+import "./UseReducer.css";
+
+const reducer = (state, actions)=>{
+     if(actions.type === "INCR"){
+         state = state + 1;
+     }
+     if(state > 0 && actions.type === "DECR"){
+         state = state -1;
+     }
+     return state;
+}
+const UseReducer = () => {
+//   const [count, setCount] = useState(0)
+     const initialValue = 0;
+     const [state, dispatch] = useReducer(reducer, initialValue)
+//   useEffect(()=>{
+//      document.title = "useReducer"   
+//   },[])
+  return (
+    <>
+      <div className="center_div">
+        <p>{state}</p>
+        <div className="button2" onClick={()=>dispatch({type:"INCR"})} >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          INCR
+        </div>
+        <div
+          className="button2" onClick={()=>dispatch({type:"DECR"})}
+          >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          DECR
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UseReducer;
